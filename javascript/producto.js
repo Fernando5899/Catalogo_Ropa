8a1789cede070cc4a552458fae421b61723aa968
Fernando5899 <f.reynosa.v023@edu.utc.mx>
2026-03-22 17:50:40 -0600

    Se añaden los archivos iniciales

---
+++ javascript/producto.js
@@ -0,0 +1,4 @@
+const imagenPrincipal = document.getElementById('imagen-principal');
+const imgModal = document.getElementById('img-modal');
+const modal = document.getElementById('miModal');
+const miniaturas = document.getElementById('.galeria imagen img')
